179c36a95ac931cc09438fa6af92ff8bf3f883d7
dingtt <1018977091@qq.com>
2020-08-02 20:28:41 +0800

    add MySQL

--- app.js
+++ app.js
@@ -5,18 +5,13 @@ const charset = require("superagent-charset");
 const superagent = charset(require("superagent"));
 const app = new Koa();
 const router = new Router();
-
-let domain = "http://www.weibo.cn";
+let domain = "http://www.baidu.com";
 let ListUrl =
-  "http://www.weibo.cn?grade=province&colcode=0302";
+  "http://www.baidu.com/sdgp2017/site/listnew.jsp?grade=province&colcode=0302";
 let detailUrl =
-  "http://www.weibo.cn?colcode=0302&id=";
-// let keywords = "工程,西楼";
-
-let keywords = "工1程,西1楼";
-// let ret = await Fruit.sync();
-// console.log("sync", ret);
-// (async () => {
+  "http://www.baidu.com/sdgp2017/site/listcontnew.jsp?colcode=0302&id=";
+let keywords = "工程,西楼";
+// let keywords = "工1程,西1楼";
 // 数据库操作
 const Sequelize = require("sequelize"); // 建立连接
 const { findOne } = require("sequelize/lib/model");
@@ -25,62 +20,71 @@ const sequelize = new Sequelize("webwatch", "ding", "xiaoyan106", {
   host: "localhost",
   port: "3307",
   dialect: "mysql",
+  dialectOptions: {
+    charset: "utf8mb4",
+    collate: "utf8mb4_unicode_ci",
+    supportBigNumbers: true,
+    bigNumberStrings: true,
+  }, //需在此处配置，否则中文无法插入
+  define: {
+    underscored: true,
+    charset: "utf8mb4",
+  },
   // operatorsAliases: false,
 }); // 定义模型
 console.log("connect database");
-const ListModel = sequelize.define("Lists", {
-  title: { type: Sequelize.STRING(100), allowNull: false },
-  // href: { type: Sequelize.FLOAT, allowNull: false },
-  id: { type: Sequelize.STRING(10), defaultValue: 0, primaryKey: true },
-  href: { type: Sequelize.STRING(100) }, // INTEGER FLOAT
-  time: { type: Sequelize.STRING(10) },
-  isnew: { type: Sequelize.INTEGER(1), defaultValue: 0 },
-  isavail: { type: Sequelize.INTEGER(1), defaultValue: 0 },
-}); // 同步数据库，force: true则会删除已存在表
+const ListModel = sequelize.define(
+  "Lists",
+  {
+    title: { type: Sequelize.CHAR(100), allowNull: false },
+    id: { type: Sequelize.STRING(10), defaultValue: 0, primaryKey: true },
+    href: { type: Sequelize.STRING(100) }, 
+    time: { type: Sequelize.STRING(10) },
+    isnew: { type: Sequelize.INTEGER(1), defaultValue: 0 },
+    isavail: { type: Sequelize.INTEGER(1), defaultValue: 0 },
+  },
+  {
+    timestamps: false,
+  }
+); 
+// 同步数据库，force: true则会删除已存在表
 console.log("create model");
-// let ret = await ListModel.sync();
-// let ret = ListModel.sync({force:true});
-let ret = ListModel.sync();
-
+let ret = ListModel.sync({ force: true });
+// let ret = ListModel.sync();
 console.log("sync1", ret);
-// })();
 
-const eachData = (arr) => {
+const eachListData = (arr) => {
   for (let i of arr) {
-    if (!i) return;
-    // let isExist = await findOne('id',i)
-    // console.log('isExist',isExist)
+    if (!i || !i.id) return;
+    console.log("list", i);
     (async () => {
-      let res = await ListModel.findOne({ where: { id: i } });
+      let res = await ListModel.findOne({ where: { id: i.id } });
       if (res) {
-        // 已记录
+        // 已记录 无需处理
       } else {
-        //
+        try {
+          let ret = await getDetail(
+            `${detailUrl}${i.id}`,
+            "#NoticeDetail",
+            keywords
+          );
+          if (ret) {
+            console.log("ret", ret);
+            if (ret.has) {
+              i.isavail = 1;
+            }
+            let insert = ListModel.create(i);
+            console.log("insert", insert);
+          }
+        } catch (err) {}
       }
     })();
   }
 };
 
-const promisify = (_this, fn) => {
-  return function (data) {
-    return new Promise((resolve, reject) => {
-      fn.call(
-        _this,
-        data,
-        (res) => {
-          resolve(res);
-        },
-        (err) => {
-          reject(err);
-        }
-      );
-    });
-  };
-};
-
 const listArr = [];
 
-const getDetail = (url,selector, keywords ) => {
+const getDetail = (url, selector, keywords) => {
   // if(!url)return
   return new Promise((resolve, reject) => {
     superagent
@@ -111,35 +115,19 @@ const getDetail = (url,selector, keywords ) => {
             }
           }
           if (has) {
-            resolve({has:has,text:text});
+            resolve({ has: has, text: text });
           } else {
-            reject(null);
+            reject();
           }
         }
       });
   });
 };
+// id是否已经存在 不存在的再获取详情 查看是否满足要求 修改标识 存入数据库
 
 router.get("/", function (ctx, context) {
   ctx.body = "搭建好了，开始吧";
-  // 测试
-  // getDetail(`${detailUrl}201601359`, "");
-  getDetail(`${detailUrl}201601359`,"#NoticeDetail" , keywords)
-    .then(
-      (res) => {
-        console.log("detail has", res);
-      },
-      (err) => {
-        console.log("has no", err);
-      }
-    )
-    .catch((err) => {
-      console.log("err err", err);
-    });
-  (async () => {
-
-  })()
-
+  // 列表页面
   superagent
     .get(ListUrl)
     .charset("utf-8")
@@ -155,44 +143,26 @@ router.get("/", function (ctx, context) {
         xmlMode: false,
         lowerCaseTags: false,
       }); //用cheerio解析页面数据
-      // const obj = {};
-      // const newlistArr = [];
       // 查看第一项是否存在
       let lis = $("#preform .news_list2 li");
       let firstLi = $("#preform .news_list2 li").eq(0);
-      //  console.log(firstLi, Object.prototype.toString.call(firstLi))
       let firstHref = firstLi.find("a").attr("href");
       let firstId = firstHref.match(/id=([0-9]+)/)[1];
-      console.log(firstId);
       if (firstId) {
         ListModel.findOne({ where: { id: firstId } }).then((res) => {
           // res首个匹配项，若没有则为null
-          console.log("findeOne1");
           if (res) {
             console.log(res.get());
           } else {
             // 遍历所有dom
             console.log("have update");
-            // const arr = eachAll($, lis);
-            // arr && eachData(arr);
+            // 数据数组
+            const arr = eachDomLi($, lis);
+            console.log("have update", arr);
+            arr && eachListData(arr);
           }
         });
-
-        // async function test2() {
-        //   console.log("test2 before");
-        //   // let data = ListModel.findOne({ where: { id: firstId } })
-        //   try {
-        //     let data = await ListModel.findOne({ where: { id: firstId } });
-        //     console.log("test2 after");
-        //     console.log("test2 after2", data);
-        //   } catch (err) {
-        //     console.log("test2 after3");
-        //     console.log("test2 after3", err);
-        //   }
-        // }
-        // test2();
       }
-
       // 查看最后一条是否存在，没有翻页page，只抓取首页，可能漏掉数据
       // console.log(listArr);
     });
@@ -200,7 +170,7 @@ router.get("/", function (ctx, context) {
 });
 
 // 遍历dom元素
-const eachAll = function (_$, eles) {
+const eachDomLi = function (_$, eles) {
   if (!eles) return;
   const listArr = [];
   eles.each((index, element) => {
